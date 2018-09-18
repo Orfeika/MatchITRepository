332200d633fcb474d3fad80c8acfa8700433d13e
orfeika <tolstenkova.a.p@gmail.com>
2018-09-18 16:23:36 +0200

    Lab5

---
+++ Lab05/src/Mole.java
@@ -0,0 +1,13 @@
+import se.lth.cs.pt.window.SimpleWindow;
+
+public class Mole {
+
+	public static void main(String[] args) {
+		System.out.println("Keep on digging!");
+		Graphics g = new Graphics(30,50,10);
+		g.square();		
+		g.block(0, 100);
+		
+	}
+
+}
