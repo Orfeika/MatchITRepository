a51ccf599c9ae0b2c7029f9b6f6a024e04573827
orfeika <tolstenkova.a.p@gmail.com>
2019-01-08 10:52:52 +0100

    Lab 5

---
+++ Java Part2/edaa01-workspace/lab5/src/Student.java
@@ -0,0 +1,20 @@
+import bst.Person;
+
+public class Student extends Person{
+	private String name;
+	private int id;
+	private String subject;
+
+	public Student(String name, int id, String subject) {
+		super(name, id);
+		this.subject = subject;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return  name + " " + id + " " + subject ;
+	}
+	
+
+}
